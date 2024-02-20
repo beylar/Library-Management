27c2442101e66cf840c1e263830229a450b4c33d
ImpuhweStella <stellahimpuhwe346@gmail.com>
2024-02-20 11:44:22 +0200

    Adding books to the library

---
+++ index.js
@@ -0,0 +1,47 @@
+var library = [
+    {
+        title: 'Business and Leadership',
+        author: 'Jane Doe',
+        availability: true,
+    },
+    {
+        title: '10 Principles of Communication',
+        author: 'Chris Malloni',
+        availability: false,
+    },
+    {
+        title: 'What is Black Market',
+        author: 'Vusi Thembekwayo',
+        availability: true,
+    },
+    {
+        title: 'Spiritual growth',
+        author: 'Myles Munroe',
+        availability: true,
+    },
+    {
+        title: 'All about BitCoin',
+        author: 'Anonymous',
+        availability: false,
+    },
+    
+
+];
+
+const addBooks = (book) =>{
+    console.log(library.length);
+    library.push(book);
+    console.log(library.length);
+           
+};
+const borrowBooks = () => {
+
+};
+const returnBooks = () => {
+
+};
+
+const listOfAvailableBooks = () => { //it should be updated accordingly
+
+};
+
